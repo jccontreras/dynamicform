06d83d9480b0cfcc8c5d8cd63dce0e93e02ea432
Ragnarok <juankcontreras00@gmail.com>
2021-05-05 17:38:37 -0500

    update project

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -3,7 +3,7 @@
     <div class="card-body">
       <h5 class="card-title">insert your data in the form please</h5>
       <h6 class="card-subtitle mb-2 text-muted">remember that the fields with * are required</h6>
-      <form id="form">
+      <form id="form" @submit.prevent="send">
         <h5 class="card-title" style="margin-top: 1em;">Personal Info</h5>
         <div class="form-group row">
           <strong class="col-sm-2 col-form-label"><label for="name">
@@ -48,20 +48,24 @@
                    placeholder="example@somthing.x" autocomplete="off" v-model="cv.email" required>
           </div>
         </div>
+        <hr>
         <h5 class="card-title" style="margin-top: 1em;">Education</h5>
         <div class="card" v-for="(edu, index) in cv.education" v-bind:key="edu">
           <div class="card-body">
-            <span class="close" @click="deleteEducation(index)">x</span>
-            <h6 class="card-subtitle mb-2 text-muted site">Site {{ index + 1 }}</h6>
+              <h6 class="card-subtitle mb-2 text-muted">Site {{ index + 1 }}</h6>
             <div class="form-group row">
               <strong class="col-sm-2 col-form-label"><label for="name">
                 Name
                 <label class="required">*</label>
               </label> </strong>
-              <div class="col-sm-10">
+              <div class="col-sm-9">
                 <input type="text" id="eduplace" class="form-control"
                        placeholder="University" autocomplete="off" v-model="edu.place" required>
               </div>
+              <strong class="col-sm-1">
+                <span class="close" @click="deleteEducation(index)"
+                      title="Delete Education" v-if="isclose">x</span>
+              </strong>
             </div>
             <div class="form-group row">
               <strong class="col-sm-2 col-form-label"><label for="name">
@@ -98,21 +102,36 @@
                 <input type="date" id="edustartdate" class="form-control"
                        placeholder="University" autocomplete="off" v-model="edu.startdate" required>
               </div>
-              <strong class="col-sm-2 col-form-label"><label for="name">
+              <strong class="col-sm-2 col-form-label" v-if="!edu.isfinish"><label for="name">
                 End Date
                 <label class="required">*</label>
               </label> </strong>
-              <div class="col-sm-3">
+              <div class="col-sm-3" v-if="!edu.isfinish">
                 <input type="date" id="eduenddate" class="form-control"
                        placeholder="University" autocomplete="off" v-model="edu.enddate" required>
               </div>
-              <strong class="col-sm-2 col-form-label"><label style="cursor: pointer"
-              title="Add Education" @click="addEducation">
+              <div class="col-sm-1">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value=""
+                         id="notfinish" v-model="edu.isfinish">
+                  <label class="form-check-label" for="notfinish">
+                    Not Finish
+                  </label>
+                </div>
+              </div>
+              <strong class="col-sm-1 col-form-label"><label style="cursor: pointer"
+                     title="Add Education" @click="addEducation">
                 +
               </label> </strong>
             </div>
           </div>
         </div>
+        <div class="alert alert-success" role="alert" v-if="sent">
+          <h4 class="alert-heading">Ready!</h4>
+          <label>We haver your data now</label> <br>
+          <label class="mb-0">We will get in contact as soon as possible.</label>
+        </div>
+        <button type="submit" class="btn btn-primary" id="enviar">Send</button>
       </form>
     </div>
   </div>
@@ -124,6 +143,8 @@ export default {
   name: 'MyForm',
   data() {
     return {
+      isclose: false,
+      sent: false,
       cv: {
         name: null,
         age: null,
@@ -136,14 +157,23 @@ export default {
             course: null,
             startdate: null,
             enddate: null,
+            isfinish: false,
           },
         ],
       },
     };
   },
+  watch: {
+    sent(val) {
+      setTimeout(() => {
+        if (val) this.sent = false;
+      }, 3000);
+    },
+  },
   methods: {
     send() {
-      alert('Se envio correctament el formulario');
+      this.isclose = false;
+      this.sent = true;
     },
     addEducation() {
       this.cv.education.push({
@@ -153,9 +183,13 @@ export default {
         startdate: null,
         enddate: null,
       });
+      this.isclose = true;
     },
     deleteEducation(index) {
       this.cv.education.splice(index, 1);
+      if (this.cv.education.length === 1) {
+        this.isclose = false;
+      }
     },
   },
 };
@@ -186,11 +220,13 @@ export default {
 
 .close {
   cursor: pointer;
-  margin-left: 45rem;
-  margin-top: 1rem;
 }
 
 .site {
   margin-right: 40rem;
+}
+
+.btn-primary {
+  margin-top: 2rem;
 }
 </style>
